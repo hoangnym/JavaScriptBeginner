5b19f3557ab300c0743699f707c748df3ae00558
hoangnym <hoangnym94@gmail.com>
2021-09-29 12:04:30 +0200

    learned about data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,4 +3,19 @@ console.log(40 + 8 + 23 - 10);
 
 // using people as examples
 let firstName = "Hoang";
-console.log(firstName)
+let PI = 3.1415
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let countryName = 'Germany';
+let countryContinent = 'Europe';
+let countryPopulation = 80000000
+
+console.log(firstName);
+console.log(countryName, countryContinent, countryPopulation)
+
+let javascriptIsFun = true;
+
+console.log(javascriptIsFun);
+console.log(typeof(javascriptIsFun));
