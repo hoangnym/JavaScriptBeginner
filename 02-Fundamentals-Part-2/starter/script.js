10447de6c6e7a68fe00c163773e57b28e9f52ee8
hoangnym <hoangnym94@gmail.com>
2021-09-30 13:29:03 +0200

    use strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+// learn about strict mode
+'use strict';
+
+let hasDriversLicense = false;
+let passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("passed the test.");
+
+// const interface = 'Audio';
+// const private = 543;
+
