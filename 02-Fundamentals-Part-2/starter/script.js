8d9018f2be775ae8ae78b0d953ad2be8e092f5bf
hoangnym <hoangnym94@gmail.com>
2021-09-30 13:37:01 +0200

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -10,3 +10,21 @@ if (hasDriversLicense) console.log("passed the test.");
 // const interface = 'Audio';
 // const private = 543;
 
+// functions
+function logger() {
+    console.log('My name is Hoang.');
+}
+
+logger(); // calling / invoking / running the function
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    return juice
+}
+
+let appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+let appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
