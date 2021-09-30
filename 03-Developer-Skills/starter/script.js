a4fb51a80cc99ee26e2e1871cc9d6ea8d548a490
hoangnym <hoangnym94@gmail.com>
2021-09-30 18:57:31 +0200

    Developer setup

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,26 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+console.log("Hello world.");
+console.log("Hello");
+console.log("What is going on.");
+
+
+// coding challenge
+let temperatures = [17, 21, 23];
+
+function printForecast(arr) {
+    let sentence = "..."
+    for (let i = 0; i < arr.length; i++) {
+        sentence += ` ${arr[i]}°C in ${i+1} ${i === 0 ? "day" : "days"} ...`
+    }
+    
+    return sentence
+}
+
+let forecast = printForecast(temperatures);
+console.log(forecast);
+
+let temperatures2 = [12, 5, -5, 0, 4];
+let forecast2 = printForecast(temperatures2);
+console.log(forecast2);
