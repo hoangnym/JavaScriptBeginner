ea9a336535c613a5ac96bcf2b2233170e1e19beb
hoangnym <hoangnym94@gmail.com>
2021-10-03 09:00:06 +0200

    add click events to modals

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-modal');
+const btnsOpenModals = document.querySelectorAll('.show-modal');
+
+function closeModal() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnsOpenModals.forEach((btn) => {
+    btn.addEventListener('click', function() {
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    })
+})
+
+closeBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// key press events
+document.addEventListener('keydown', function(event) {
+    if (event.key === "Escape" && !modal.classList.contains('hidden')) {
+        closeModal();
+    };
+})
