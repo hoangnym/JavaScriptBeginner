8dffeee77e3d79cd4df72da2e5e9c82b4030309e
hoangnym <hoangnym94@gmail.com>
2021-10-05 08:18:45 +0200

    scoping

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2021 - birthYear;
+    
+    function printAge() {
+        const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+        console.log(output);
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            var milennial = true;
+            const firstName = "Steven"; // looks for current scope first
+            const str = `Oh, you are a milennial, ${firstName}`;
+            console.log(str);
+
+            function add(a, b) {return a + b};
+        }
+        // console.log(str); // block scoped
+        console.log(milennial); // function scoped
+        // add(2, 3); // block scoped
+    }
+
+    printAge();
+
+    return age;
+}
+
+const firstName = "Hoang";
+let age = calcAge(1994);
+console.log(age);
