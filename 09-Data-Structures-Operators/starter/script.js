8f0b081954ed5bfb5eec23cc7d110f1d35e721f3
hoangnym <hoangnym94@gmail.com>
2021-10-05 23:05:47 +0200

    destructure arrays

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,6 +26,10 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  }
 };
 
 const arr = [2, 3, 4];
@@ -34,5 +38,32 @@ const b = arr[1];
 const c = arr[2];
 
 const [x, y, z] = arr;
+console.log(x, y, z);
 
-console.log(x, y, z)
+let [main, ,secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// Switching variables
+// let temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main]
+console.log(main, secondary);
+
+
+// Destructuring
+let [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// Nested destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, ,j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default values
+const [p=1, q=1, r=1] = [8, 9];
+console.log(p, q, r);
