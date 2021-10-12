bbd1dc26bd873d0d4878e929b5d18d6cc0b63f1d
hoangnym <hoangnym94@gmail.com>
2021-10-12 11:55:23 +0200

    combine string and array method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -471,4 +471,24 @@ const sums = accounts
 
 console.log(sums);
 
+// 4.
+// this is a nice title -> This Is a NICE Title
+const convertTitleCase = function (title) {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (!exceptions.includes(word) ? capitalize(word) : word))
+    .join(' ');
+
+  return capitalize(titleCase);
+};
+
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+
 /////////////////////////////////////////////////
