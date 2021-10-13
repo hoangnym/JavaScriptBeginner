bb82395765aa8e3380366ae6de480b96845d2265
hoangnym <hoangnym94@gmail.com>
2021-10-13 19:02:44 +0200

    add intersection observer api to section revealing

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -158,13 +158,32 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+// Revealing sections on scroll
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
 //////////////////// EXPERIMENTS ////////////////////
 // Selecting documents
 // console.log(document.documentElement);
 // console.log(document.head);
 // console.log(document.body);
-
-const allSections = document.querySelectorAll('.section');
 // console.log(allSections);
 
 document.getElementById('section--1');
