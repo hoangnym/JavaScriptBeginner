805e59d9cc24e708ab0c49d3bfa5b5ccebcf9777
hoangnym <hoangnym94@gmail.com>
2021-10-13 11:29:08 +0200

    create, move and delete dom elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,38 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Selecting documents
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We used cookies for improved functionality and analytics.'
+message.innerHTML =
+  'We used cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+console.log(message);
+
+header.append(message); // first child of header element
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
