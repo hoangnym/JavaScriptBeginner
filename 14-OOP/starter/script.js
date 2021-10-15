9bbeee7523f263b764e8c311b3960dcddf1104b8
hoangnym <hoangnym94@gmail.com>
2021-10-15 11:57:09 +0200

    class declarations

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -97,3 +97,38 @@ bmw.brake();
 mercedes.accelerate();
 mercedes.brake();
 mercedes.brake();
+
+// Using syntactic sugar
+// class expression
+// const PersonCl = class {}
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  displayName() {
+    console.log(this.firstName);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1990);
+console.log(jessica);
+jessica.calcAge();
+jessica.displayName();
+
+PersonCl.prototype.greet = function () {
+  console.log(`Hey ${this.firstName}`);
+};
+
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Class are first-class citizens
+// 3. Classes are executed in strict mode
