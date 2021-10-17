c58565927e33c781c1771f0b55b04bde10801999
hoangnym <hoangnym94@gmail.com>
2021-10-17 15:46:04 +0200

    encapsulation

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -206,26 +206,47 @@ class Account {
     this.owner = owner;
     this.currency = currency;
     this.pin = pin;
-    this.movements = [];
+    // protected property
+    this._movements = [];
     this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${this.owner}`);
   }
 
   // Public interface
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
+  }
+
+  _approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+    }
   }
 }
 
 const acc1 = new Account('Jonas', 'EUR', 1111);
 console.log(acc1);
 
+acc1._movements.push(250);
+acc1._movements.push(-140);
 acc1.deposit(250);
 acc1.withdraw(140);
+acc1.requestLoan(1000);
 
 console.log(acc1);
+console.log(acc1.pin);
+console.log(acc1.getMovements());
