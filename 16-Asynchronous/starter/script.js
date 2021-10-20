8f12360ac082ae8ce2986f5cd1d97d21bf38e8a5
hoangnym <hoangnym94@gmail.com>
2021-10-20 22:56:00 +0200

    promisifying

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -137,7 +137,6 @@ const getJSON = function (url, errorMsg = 'Something went wrong') {
 const getCountryData = function (country) {
   // Return promise => pass callback function in then method
   getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found')
-    .then()
     .then(data => {
       renderCountry(data[0]);
       const neighbour = data[0].borders?.[0] ?? false;
@@ -174,8 +173,73 @@ btn.addEventListener('click', function () {
 // getCountryData('asjaosja');
 
 // Coding Challenge 1
-const reverseGeocoding
+let lat = 48.84196;
+let lng = 10.10336;
 
-const whereAmI = function(lat, lng) {
-    reverseGeocoding(url)
-}
+const reverseGeocoding = function (url, errorMsg = 'Problem with geocoding') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})!`);
+    return response.json();
+  });
+};
+
+const whereAmI = function (lat, lng) {
+  reverseGeocoding(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(data => {
+      console.log(`You are in ${data.city}, ${data.country}`);
+      getCountryData(data.country.toLowerCase());
+      return data;
+    })
+    .catch(err => {
+      console.error(err.message);
+    });
+};
+
+// whereAmI(lat, lng);
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
+
+/// Event Loop
+// console.log('Test start');
+// setTimeout(() => console.log('0 sec timer'), 0);
+// Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+// console.log('Test end');
+
+// Building a simple promise with an executor
+const lotteryPromise = new Promise(function (resolve, reject) {
+  // contain asynchronous behaviour that consumes the promise
+  console.log('Lottery draw is happening ...');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      // mark as fulfilled value
+      resolve('You WIN');
+    } else {
+      // mark as failed value
+      reject(new Error('You LOST your money.'));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+// chain of asynchronous behaviour
+wait(2)
+  .then(() => {
+    console.log('I waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for 1 second');
+  });
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem!')).catch(x => console.error(x));
